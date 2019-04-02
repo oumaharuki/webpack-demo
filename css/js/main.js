11e71b14355690b8a1d2d7b93bd446329f38e12a
moqinglong <ml@123456>
2019-04-02 16:48:30 +0800

    css-loader

---
+++ css/js/main.js
@@ -0,0 +1,3 @@
+import "../css/index.css";
+// import "../css/index.sass";
+document.body.innerText="holle world!";
---
+++ css/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports={
+    mode: 'development',
+    entry:"./js/main.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"],
+                include: path.resolve(__dirname, 'css'),
+                exclude: path.resolve(__dirname, 'node_modules')
+            },
+        ]
+    }
+}
